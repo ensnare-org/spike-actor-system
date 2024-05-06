09d52042b75250f49e20833147a0325f82f5f0ea
Mike Tsao <mike@sowbug.com>
2024-05-06 13:21:41 -0700

    quieter, simpler

--- src/busy.rs
+++ src/busy.rs
@@ -1,4 +1,4 @@
-use ensnare::{prelude::*, util::Rng};
+use ensnare::prelude::*;
 use ensnare_proc_macros::{IsEntity, Metadata};
 use serde::{Deserialize, Serialize};
 
@@ -6,18 +6,13 @@ use serde::{Deserialize, Serialize};
 #[entity(Controls, TransformsAudio)]
 pub struct BusyWaiter {
     uid: Uid,
-    #[serde(skip)]
-    rng: Rng,
 }
 impl Serializable for BusyWaiter {}
 impl HandlesMidi for BusyWaiter {}
 impl Generates<StereoSample> for BusyWaiter {
-    fn generate(&mut self, values: &mut [StereoSample]) -> bool {
+    fn generate(&mut self, _values: &mut [StereoSample]) -> bool {
         let aaa: usize = (0..=100000).sum();
         println!("{aaa}"); // Don't remove this; it keeps the work from being optimized away
-        for v in values.iter_mut() {
-            *v = StereoSample::from(self.rng.rand_float());
-        }
         true
     }
 }
